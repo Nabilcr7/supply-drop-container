75ff3512067186dcda645914028b99da445d14f4
Nabilcr7 <74101766+Nabilcr7@users.noreply.github.com>
2021-01-01 23:27:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,76 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	wall1Sprite=createSprite(400,655,160,10)
+	wall1Sprite.shapeColor=color(255,105,180)
+	wall2Sprite=createSprite(485,635,10,50)
+	wall2Sprite.shapeColor=color(255,69,0)
+    wall3Sprite=createSprite(320,635,10,50)
+	wall3Sprite.shapeColor=color(255,69,0)
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+	
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background("yellow");
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    Matter.Body.setStatic(packageBody,false);
+    Matter.Body.setStatic(wall1,true)
+	Matter.Body.setStatic(wall2,true)
+	Matter.Body.setStatic(wall3,true)
+  }
+}
+
+
+
